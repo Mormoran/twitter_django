9b3e153c10f0d94ea530a91c814e618835845eac
Andres Correa <mormoran@gmail.com>
2019-12-21 23:36:22 +0000

    Added default array callable for array field.

--- accounts/models.py
+++ accounts/models.py
@@ -2,9 +2,11 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
+def default_array():
+    return []
 
 class CustomUser(AbstractUser):
-    searched_users = ArrayField(models.CharField(max_length=15, blank=False), null=True, blank=True)
+    searched_users = ArrayField(models.CharField(max_length=15, blank=False), null=True, blank=True, default=default_array)
 
     def __str__(self):
         return self.username
