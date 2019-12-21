d06d5e3881a63f065b9716aaa4bfcfad2a11c3a4
Andres Correa <mormoran@gmail.com>
2019-12-21 18:06:51 +0000

    Added default empty array.

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    searched_users = ArrayField(models.CharField(max_length=15, blank=False, null=True, default=[]))
+    searched_users = ArrayField(models.CharField(max_length=15, blank=True, null=True, default=[]))
 
     def __str__(self):
         return self.username
